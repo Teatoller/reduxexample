b2c0623a236854f23470bdf8c5af66cdb9d21299
Teatoller <steven.otieno1@gmail.com>
2019-07-14 20:37:20 +0300

    (API): fetch posts
    - add a fetch API function
    - add props
    - render title and message body
    - create post form
    - add onchange method
    - add onsubmit method
    - bind the events
    
    [Starts #001]

---
+++ src/components/PostForm.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+
+class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    };
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(post)
+    })
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            Title:
+            <br />
+            <input
+              type="text"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+            />
+          </div>
+          <br />
+          <div>
+            Body:
+            <br />
+            <textarea
+              type="text"
+              name="body"
+              onChange={this.onChange}
+              value={this.state.body}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default PostForm;
