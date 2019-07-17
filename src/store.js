417fd0ca054e56cb97dca84b3eac3b7cc5d15197
Teatoller <steven.otieno1@gmail.com>
2019-07-17 10:54:56 +0300

    (redux): implement redux
    - add compose and redux to store
    
    [Starts #005]

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 
@@ -6,6 +6,13 @@ const initialeState = {};
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialeState, applyMiddleware(...middleware));
+const store = createStore(rootReducer, 
+    initialeState, 
+    compose (
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+    
+    );
 
 export default store;
